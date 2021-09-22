6b4396171e04c77c2335a0810d31582d5355a3a5
hasibulalamrahi <hasibulalam6919@gmail.com>
2021-09-23 04:50:17 +0600

    new updates are pushed here

--- src/components/categoryPage.vue
+++ src/components/categoryPage.vue
@@ -17,7 +17,7 @@
                     <tr v-for="(cat,index) in categories" :key="cat.id">
                         <td>{{cat.name}}</td>
                         <td>
-                            <button type="button" class="btn btn-light mr-1" @click="openEdit(cat)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <button type="button" class="btn btn-light mr-1" @click="openEdit(cat,index)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                             </svg>
@@ -139,9 +139,9 @@ export default{
                 this.allData ='';
 
             },
-            openEdit(cat){
+            openEdit(cat,index){
                 
-                this.editId = cat;
+                this.editId = index;
                 this.dynamicTitle="Edit Category";
                 this.myModel = true;
                 this.allData = cat.name ;
@@ -172,14 +172,14 @@ export default{
                 
                 this.categories.splice(index,1);
 
-                 console.log(index);
+                //  console.log(index);
                  localStorage.setItem(STORAGE_KEY,JSON.stringify(this.categories));
 
             },
             editData(){
                 this.EditData = this.allData;
-                const ide =this.editId.id;
-                // alert("Data is edited");
+                let ide =this.editId;
+                console.log(ide);
                 if(this.EditData.trim().length == 0){
                     this.myModel = false
                 }
@@ -189,9 +189,9 @@ export default{
                     this.categories[ide].name = this.EditData;
                     this.myModel = false;
                     this.editId = '';
+                    localStorage.setItem(STORAGE_KEY,JSON.stringify(this.categories));
 
                 }
-                // localStorage.setItem(STORAGE_KEY,JSON.stringify(this.categories));
 
               
 
